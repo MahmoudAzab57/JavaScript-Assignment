2b20721fca138bec9b9060a4bb7c59c45634ef29
MahmoudAzab57 <mahmoudazab57@gmail.com>
2023-02-26 17:43:18 +0200

    Done

---
+++ script.js
@@ -0,0 +1,134 @@
+//Q1
+
+// var x=prompt("Write a number")
+// console.log(x)
+
+//Q2
+
+// var x=prompt("Write a number")
+// if(x%3==0 && x%4==0){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+        
+//Q3
+        
+// var x=prompt("Write first number")
+// var y=prompt("Write second number")
+// if(x>y){
+//     console.log(x)
+// }else if(y>x){
+//     console.log(y)
+// }
+        
+//Q4
+// var x=prompt("Write a number")
+// x=x*-1
+// console.log(x)
+
+
+//Q5!!!!!!!!!!!!!!!!!!111
+// var x=prompt("Write first number")
+// var y=prompt("Write second number")
+// var z=prompt("Write thrid number")
+// if(x>y && x>z){
+//     console.log("max num is", x)
+// }else if(y>x && y>z){
+//     console.log("max num is", y)
+// }else if(z>x && z>y){
+//     console.log("max num is", z)
+// }
+
+//Q6
+// var x=prompt("Write a number")
+// if(x%2==0){
+//     console.log("even")
+// }else{console.log("odd")}
+
+//Q8
+// var x=prompt("Write a character")
+// if(x=="a"){
+//     console.log("vowel")
+// }else if(x=="e"){
+//     console.log("vowel")
+// }else if(x=="i"){
+//     console.log("vowel")
+// }else if(x=="o"){
+//     console.log("vowel")
+// }else if(x=="u"){
+//     console.log("vowel")
+// }
+// else {
+//     console.log("consonant")
+// }
+
+//Q9
+// var x=prompt("Write a number")
+// for(var i=1;i<=x;i++){
+//     console.log(i)
+// }
+
+//Q10
+// var x=prompt("Write a number")
+// for(var i=1;i<=12;i++){
+//     console.log(x*i)
+// }
+
+//Q11
+// var x=prompt("Write a number")
+// for(var i=0;i<=x;i+=2){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+//Q12
+// var x=prompt("Write first number")
+// var y=prompt("Write second number")
+// console.log(Math.pow(x,y))
+
+//Q12
+// var a=prompt("Write first mark")
+// var b=prompt("Write second mark")
+// var c=prompt("Write third mark")
+// var d=prompt("Write forth mark")
+// var e=prompt("Write fifth mark")
+// console.log(Number(a)+Number(b)+Number(c)+Number(d)+Number(e))
+// console.log((Number(a)+Number(b)+Number(c)+Number(d)+Number(e)) /5)
+// console.log((Number(a)+Number(b)+Number(c)+Number(d)+Number(e))*100 /500)
+
+//Q13
+// var x=prompt("Write month number")
+//     x=Number(x)
+// if(x==1 || x==3 || x==5 || x==7 || x==8 || x==10 || x==12){
+//     console.log("31 days")
+// }else if(x==4 || x==6 || x==9 || x==11){
+//     console.log("30 days")
+// }else if(x=2){
+//     console.log("28 or 29 days")
+// }
+
+//Q14
+// var a=prompt("Write Physics mark")
+// var b=prompt("Write Chemistry mark")
+// var c=prompt("Write Biology mark")
+// var d=prompt("Write Mathematics mark")
+// var e=prompt("Write Computer mark")
+// var x=((Number(a)+Number(b)+Number(c)+Number(d)+Number(e))*100 /500)
+// console.log("your percentage" ,x)
+// if(a>=90){
+//     console.log("Grad A")
+// }else if(x>=80 && a<90){
+//     console.log("Grad B")
+// }else if(x>=70 && a<80){
+//     console.log("Grad C")
+// }else if(x>=60 && a<70){
+//     console.log("Grad D")
+// }else if(x>=40 && a<60){
+//     console.log("Grad E")
+// }else if(x<40){
+//     console.log("Grad F")
+// }
+
+//Q15
